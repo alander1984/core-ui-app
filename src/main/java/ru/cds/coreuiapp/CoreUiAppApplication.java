2008427f6028906b065828754776f480ad89cb90
Alander Khrenov <a.hrenov@mail.com>
2019-02-22 11:42:36 +0000

    first commit

---
+++ src/main/java/ru/cds/coreuiapp/CoreUiAppApplication.java
@@ -0,0 +1,13 @@
+package ru.cds.coreuiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreUiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreUiAppApplication.class, args);
+	}
+
+}
