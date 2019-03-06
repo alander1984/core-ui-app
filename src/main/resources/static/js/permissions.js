d1d4ac8b7145ea1b6d5d7cdc550c3927efebdad1
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-03-06 10:04:33 +0000

    Permissions CRUD

--- src/main/resources/static/js/permissions.js
+++ src/main/resources/static/js/permissions.js
@@ -2,11 +2,21 @@ Vue.component('permissions', {
     
     template: '<div>' +
                 '<h3>Permissions</h3>' +
-                '<b-button id="add" variant="primary" v-on:click="addPermission">Add Permission</b-button>' +
+                '<b-modal id="modalAdd" ref="modal1" title="Add Permission" @ok="handleOk" @shown="clearName">' +
+                  '<form @submit.stop.prevent="handleSubmit">' +
+                    '<b-form-input type="text" placeholder="Enter permission" v-model="permission" />' +
+                  '</form>' +
+                '</b-modal>' +
+                '<b-modal id="modalEdit" ref="modal" title="Edit Permission" @ok="handleOkEdit" @shown="iPermission">' +
+                          '<form @submit.stop.prevent="handleSubmitEdit">' +
+                            '<b-form-input type="text"  v-model="permission" />' +
+                          '</form>' +
+                        '</b-modal>' +
+                '<b-button id="add" variant="primary" v-b-modal.modalAdd>Add Permission</b-button>' +
                 '<br /><br />' +
                 '<div v-for="(item, index) in listPermissions" :key=item.index>' +
-                        '{{ index }}. {{item}} <b-button variant="danger" v-on:click="deletePermission(index)" >X</b-button>&nbsp;&nbsp;' +
-                        '<b-button variant="success" v-on:click="editPermission(index)" >EDIT</b-button>' +
+                        '{{ index + 1 }}. {{item}} <b-button variant="danger" v-on:click="deletePermission(index)" >X</b-button>&nbsp;&nbsp;' +
+                        '<b-button variant="success" v-b-modal.modalEdit v-on:click="editPermission(index)" >EDIT</b-button>' +
                     '</div>' +
               '</div>',
     data(){
@@ -18,19 +28,66 @@ Vue.component('permissions', {
                         role: String,
                         permissionsForRole: []
                     }
-                ]
+                ],
+                permission: '',
+                index: Number
             }
         },
     methods: {
-        addPermission(){
-            alert("Добавить");
-        },
         deletePermission(index){
-            alert("Удалить  № " + index);
+            //alert("Удалить  № " + index);
+            this.listPermissions.splice(index, 1);
         },
         editPermission(index){
-            alert("Редактировать  № " + index);
-        }
+            //alert("Редактировать  № " + index);
+            this.permission = this.listPermissions[index];
+            this.index = index;
+        },
+        clearName() {
+            this.permission = '';
+        },
+        handleOk(evt) {
+            // Prevent modal from closing
+            evt.preventDefault();
+            if (!this.permission) {
+              alert('Please enter permission');
+            } else {
+              this.handleSubmit();
+            }
+        },
+        handleSubmit() {
+            this.listPermissions.push(this.permission);
+            this.clearName();
+            this.$nextTick(() => {
+              // Wrapped in $nextTick to ensure DOM is rendered before closing
+              console.log("********   " + this.$refs.modal);
+              this.$refs.modal1.hide();
+             // document.getElementById('modalAdd').style.display= 'none';
+            });
+        },
+        handleOkEdit(evt) {
+            // Prevent modal from closing
+            evt.preventDefault();
+            if (this.permission === '') {
+              alert('Please enter permission');
+            } else {
+              this.handleSubmitEdit();
+            }
+        },
+        handleSubmitEdit() {
+            Vue.set(this.listPermissions, this.index, this.permission);
+            this.clearName();
+            this.$nextTick(() => {
+              // Wrapped in $nextTick to ensure DOM is rendered before closing
+              this.$refs.modal.hide();
+            });
+        },
+        iPermission(){
+            
+        }        
+    },
+    created(){
+       /* this.listPermissions = null; *//* Здесь запрос по списку Permissions */
     }
     
 });
