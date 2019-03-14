d6ec18ad330fa4af870edff8125730f294ee2b48
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-03-14 12:35:33 +0000

    page Roles

--- src/main/resources/static/js/roles.js
+++ src/main/resources/static/js/roles.js
@@ -2,16 +2,43 @@ const roles = Vue.component('roles', {
 
     template:  '<div>' +
                     '<h3>Roles</h3>' +
-                    '<div v-for="(item, index) in listRoles" :key=item.index>' +
-                        '{{ index }}. {{item}}' +
+                    '<div v-for="(item, index) in rolesPermissions" :key=item.index @click="permissions(index)" v-bind:style="{cursor: pointer, color:color}">' +
+                        '{{ index + 1}}. {{item.role}}' +
+                    '<br /></div>' +
+                    '<br />' +
+                    '<br />' +
+                    '<h4 v-if="show == true">Permissions for <span style="color: red;">{{role}}</span></h4>' +
+                    '<div v-for="(item, index) in listPermissions" :key=item.index style="color: green;">' +
+                        '{{ index + 1 }}. {{item}}' +
                     '</div>' +
                 '</div>',
     data(){
             return {
-                listRoles: ['admin', 'user', 'driver']
+                listPermissions: [],
+                rolesPermissions: [
+                    {
+                        role: String,
+                        permissionsForRole: []
+                    }
+                ],
+                role: '',
+                show: false,
+                color: 'maroon',
+                pointer: 'pointer'
             }
         },
     created(){
        /* this.listRoles = null; *//* Здесь запрос по списку ролей */
+        this.rolesPermissions = [{role:'admin', permissionsForRole:['perm1', 'perm2', 'perm4', 'perm5']},
+                                 {role:'user', permissionsForRole:['perm1', 'perm3', 'perm4']},
+                                 {role:'boss', permissionsForRole:['perm3', 'perm4']}];
+    },
+    methods: {
+        permissions(index){
+            this.listPermissions = this.rolesPermissions[index].permissionsForRole;
+            this.role = this.rolesPermissions[index].role;
+            this.show = true;
+        }
+
     }
 });
