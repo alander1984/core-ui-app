885c29007a3fe1f3b9c44d46c4b62ddac44110e6
Roman Sergienko <roman.sergienko@taskdata.com>
2019-03-20 17:58:57 +0400

    CDS-43 Left menu slide

--- src/main/resources/static/js/slide.js
+++ src/main/resources/static/js/slide.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
+  $("#slide-down-menu-button").hide();
+
   $('#slide-right').click(function () {
     $('.content-wrapper').slideDown();
   });
 
   $('#slide-left').click(function () {
     $('.map-wrapper').slideDown();
+  });
+
+  $("#slide-up-menu-button").click(function (e) {
+    $("#slide-up-menu-button").hide();
+    $(".sidenav").width(250);
+    $("#slide-down-menu-button").show("slow");
+  });
+
+  $("#slide-down-menu-button").click(function (e) {
+    $("#slide-down-menu-button").hide();
+    $(".sidenav").width(50);
+    $("#slide-up-menu-button").show("slow");
   });
 });
 
