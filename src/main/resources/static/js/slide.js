f51edf9a289e9f441d46411242583a81e0563386
Roman Sergienko <roman.sergienko@taskdata.com>
2019-03-21 11:43:30 +0400

    CDS-43 Slide main areas

--- src/main/resources/static/js/slide.js
+++ src/main/resources/static/js/slide.js
@@ -1,27 +1,44 @@
 $(document).ready(function () {
   console.log("ready!");
-  $("#slide-down-menu-button").hide();
+  var map_wrapper = $('#map-wrapper');
+  var content_wrapper = $('.content-wrapper');
+  var slide_down_menu_button = $("#slide-down-menu-button");
+  var slide_up_menu_button = $("#slide-up-menu-button");
+
+  slide_down_menu_button.hide();
 
   $('#slide-right').click(function () {
-    $('#map-wrapper').css('min-width', '100%');
-    $('.content-wrapper').css('min-width', '0%');
+    if (map_wrapper.width() < 50) {
+      map_wrapper.css('min-width', '50%');
+      content_wrapper.css('min-width', '50%');
+    } else {
+      map_wrapper.css('min-width', '100%');
+      content_wrapper.css('min-width', '0%');
+    }
   });
 
   $('#slide-left').click(function () {
-    $('#map-wrapper').css('min-width', '2.67%');
-    $('.content-wrapper').css('min-width', '100%');
+    if (content_wrapper.width() < 50){
+      map_wrapper.css('min-width', '50%');
+      content_wrapper.css('min-width', '50%');
+    } else {
+      map_wrapper.css('min-width', '2.67%');
+      map_wrapper.css('width', '0');
+      content_wrapper.css('min-width', '100%');
+    }
+
   });
 
-  $("#slide-up-menu-button").click(function (e) {
-    $("#slide-up-menu-button").hide();
+  slide_up_menu_button.click(function (e) {
+    slide_up_menu_button.hide();
     $(".sidenav").width(250);
-    $("#slide-down-menu-button").show("slow");
+    slide_down_menu_button.show("slow");
   });
 
-  $("#slide-down-menu-button").click(function (e) {
-    $("#slide-down-menu-button").hide();
+  slide_down_menu_button.click(function (e) {
+    slide_down_menu_button.hide();
     $(".sidenav").width(50);
-    $("#slide-up-menu-button").show("slow");
+    slide_up_menu_button.show("slow");
   });
 });
 
