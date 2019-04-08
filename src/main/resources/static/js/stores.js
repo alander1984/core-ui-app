4352ffad8e6f8fab2c7f9b068def86f2904058d4
Yaroslav Korzhev <ykorzhev@egartech.com>
2019-04-08 10:51:51 +0000

    stores ui

--- src/main/resources/static/js/stores.js
+++ src/main/resources/static/js/stores.js
@@ -1,43 +1,29 @@
-const stores = Vue.component ('stores', {
+var Stores = Vue.component ('stores', {
     
     
     template: '<div>'
 	+ '<h3>Магазины</h3>'
-	+ '<b-modal id="modalAddStore" ref="modal_add_store" title="Добавить магазин"  @ok="handleOkAddStore" @shown="clearName">'
-	+	'<form @submit.stop.prevent="handleSubmitAddStore">'
-	+		'<b-form-input type="text" placeholder="Тип магазина" v-model="storeType" /><br/>'
+	+ '<b-modal id="modalAddStore" ref="modal_add_store" title="Добавить магазин"  @ok="handleOk" @shown="clearName">'
+	+	'<form @submit.stop.prevent="handleSubmit">'
+	+		'<b-form-input type="number"  v-model="storeId" style="display: none;"/>'
+	+		'<b-form-select :options="storeTypeOptions" v-model="storeType"></b-form-select><br/><br/>'
 	+		'<b-form-input type="text" placeholder="Название магазина" v-model="storeName" /><br/>'
 	+		'<b-form-input type="text" placeholder="Адрес" v-model="storeAddress" /><br/>'
 	+		'<b-form-input type="text" placeholder="Код" v-model="storeCode" /><br/>'
 	+		'<b-form-input type="number" placeholder="Долгота" v-model="storeLon" /><br/>'
 	+		'<b-form-input type="number" placeholder="Широта" v-model="storeLat" /><br/>'
 	+		'<b-form-input type="text" placeholder="Комментарий" v-model="storeComment" /><br/>'
+	+       '<div id="addModalMapContainer" @click="setCoords"><div id="YMapsID" style="width:100%;height:300px;"></div></div>'
 	+	'</form>'
-	+ '</b-modal>'
-	+ '<b-modal id="modalEditStore" ref="modal_edit_store" title="Редактирование магазина" @ok="handleOkEditStore">'
-	+	'<form @submit.stop.prevent="handleSubmitEditStore">'
-	+		'<b-form-input type="number"  v-model="storeId" style="display: none;"/><br/>'
-	+		'<b-form-input type="text" placeholder="Тип магазина" v-model="storeType" /><br/>'
-	+		'<b-form-input type="text" placeholder="Название магазина" v-model="storeName" /><br/>'
-	+		'<b-form-input type="text" placeholder="Адрес" v-model="storeAddress" /><br/>'
-	+		'<b-form-input type="text" placeholder="Код" v-model="storeCode" /><br/>'
-	+		'<b-form-input type="number" placeholder="Долгота" v-model="storeLon" /><br/>'
-	+		'<b-form-input type="number" placeholder="Широта" v-model="storeLat" /><br/>'
-	+		'<b-form-input type="text" placeholder="Комментарий" v-model="storeComment" /><br/>'
-	+   '</form>'
-	+   '<section id="map-wrapper" layout:fragment="map-wrapper">'
-    +        '<div id="YMapsID" style="width: 100%; height: 100%;"></div>'
-    +       '<i id="slide-left" class="fas fa-caret-left fa-3x"></i>'
-    +    '</section>'
 	+ '</b-modal>'
 
 	+ '<b-button id="addStoreButton" variant="primary" v-b-modal.modalAddStore>Добавить</b-button>'
-	+ '<br/>'
+	+ '<br/><br/>'
 	
-	+ '<table style="border: 2px solid black; width: 60%;">'
-	+   '<tr style="border: 1px solid black;"><td>№</td><td>Тип</td><td>Название</td><td>Адрес</td><td>Код</td><td>Долгота</td><td>Широта</td><td>Комментарий</td><td></td><td></td></tr>'
-	+   '<tr v-for="(item, index) in storesList" :key=item.index style="border: 1px solid black;">'
-	+		'<td>{{ index + 1 }}</td>'
+	+ '<table class="table table-hover table-sm table-bordered">'
+	+   '<thead><tr><th scope="col">№</th><th scope="col">Тип</th><th scope="col">Название</th><th scope="col">Адрес</th><th scope="col">Код</th><th scope="col">Долгота</th><th scope="col">Широта</th><th scope="col">Комментарий</th><th scope="col"></th><th scope="col"></th></tr></thead>'
+	+   '<tbody><tr v-for="(item, index) in storesList" :key=item.index>'
+	+		'<th scope="row">{{ index + 1 }}</th>'
 	+		'<td>{{item.type}}</td>'
 	+		'<td>{{item.name}}</td>'
 	+		'<td>{{item.address}}</td>'
@@ -46,8 +32,8 @@ const stores = Vue.component ('stores', {
 	+		'<td>{{item.lat}}</td>'
 	+		'<td>{{item.comment}}</td>'
 	+		'<td><b-button variant="danger" v-on:click="deleteStore(index, item)">X</b-button></td>'
-	+		'<td><b-button variant="success" v-b-modal.modalEditStore v-on:click="editStore(index)">Изменить</b-button></td>'
-	+	'</tr>'
+	+		'<td><b-button variant="success" v-b-modal.modalAddStore v-on:click="editStore(index)">Изменить</b-button></td>'
+	+	'</tr></tbody>'
 	+ '</table>'
     + '</div>',
     
@@ -55,19 +41,22 @@ const stores = Vue.component ('stores', {
     data() {
         return {
             storesList: [],
-            storeId: Number,
+            storeId: 0,
             storeType: '',
             storeName: '',
             storeAddress: '',
             storeCode: '',
-            storeLon: Number,
-            storeLat: Number,
+            storeLon: 0,
+            storeLat: 0,
             storeComment: '',
-            index: Number
-        }
+            index: Number,
+            storeTypeOptions: [
+               { value: null, text: 'Тип магазина'},
+               { value: 'OFFLINE', text: 'Офлайн' },
+               { value: 'ONLINE', text: 'Онлайн' }
+            ]
+        };
     },
-    
-    
     methods: {
         deleteStore(index, item) {
             console.log("DELETE STORE")
@@ -77,6 +66,7 @@ const stores = Vue.component ('stores', {
             this.storesList.splice(index, 1);
             
         },
+        
         editStore(index) {
             this.storeId = this.storesList[index].id;
             this.storeType = this.storesList[index].type;
@@ -88,13 +78,38 @@ const stores = Vue.component ('stores', {
             this.storeComment = this.storesList[index].comment;
             this.index = index;        
         },
-        handleOkAddStore(evt) {
+        
+        handleOk(evt) {
             evt.preventDefault();
             //обработка пустых полей
-            this.handleSubmitAddStore();
+            this.handleSubmit();
         },
-        handleSubmitAddStore() {
-            console.log("ADD STORE");
+        
+        handleSubmit() {
+            console.log('###B-MODAL:' + this.storeId);
+            if(this.storeId == 0) {
+                this.addStore();
+            } else {
+                this.updateStore();
+            }            
+        },
+        
+        clearName() {
+            console.log("CLEAR NAME ID: " + this.storeId);
+            if(this.storeId == 0) {
+                this.storeId = 0;
+                this.storeType = '';
+                this.storeName = '';
+                this.storeAddress = '';
+                this.storeCode = '';
+                this.storeLon = 0;
+                this.storeLat = 0;
+                this.storeComment = '';
+            }   
+        },
+        
+        addStore() {
+            console.log('###ADD STORE');
             newStore = new Object();
             newStore.type = this.storeType;
             newStore.name = this.storeName;
@@ -103,34 +118,20 @@ const stores = Vue.component ('stores', {
             newStore.lon = this.storeLon;
             newStore.lat = this.storeLat;
             newStore.comment = this.storeComment;
-            
+            console.log('add store ' + newStore.lon);
             CDSAPI.Stores.createOrUpdateStore(newStore).then(id => {
                 console.log("STORE CREATED WITH ID: " + id);
                 newStore.id = id.array[0];
                 this.storesList.push(newStore);
+                this.clearName();
             });
             this.$nextTick(() => {          
               this.$refs.modal_add_store.hide();
-            });          
+            });
         },
-        clearName() {
-            this.type = '';
-            this.name = '';
-            this.address = '';
-            this.code = '';
-            this.lon = 0;
-            this.lat = 0;
-            this.comment = '';
-            console.log("!!!!!!!!!!CLEAR NAME");
-            
-        },
-        handleOkEditStore(evt) {
-            evt.preventDefault();
-            //обработка пустых полей
-            this.handleSubmitEditStore();
-        },
-        handleSubmitEditStore() {
-            console.log("EDIT STORE!")
+        
+        updateStore() {
+            console.log('###UPDATE STORE ' + this.storeId);
             editableStore = new Object();
             editableStore.id = this.storeId;
             editableStore.type = this.storeType;
@@ -144,24 +145,30 @@ const stores = Vue.component ('stores', {
             CDSAPI.Stores.createOrUpdateStore(editableStore).then(id => {
                 console.log("STORE UPDATED WITH ID: " + id);
             });
+            this.clearName();
             this.$nextTick(() => {
               // Wrapped in $nextTick to ensure DOM is rendered before closing
-              this.$refs.modal_edit_store.hide();
+              this.$refs.modal_add_store.hide();
             });
         },
+       
         getAllStores() {
             console.log("GET ALL STORES!");
             CDSAPI.Stores.getAllStore().then(stores => {
                 this.storesList = stores;
-            });
-                
-        }
-         
+            });               
+        },
         
+        setCoords() {
+            this.storeLon = clickedCoords[0];
+            this.storeLat = clickedCoords[1];
+        }
     },
     
     created() {
         this.getAllStores();
     }
 });
+
+
 
