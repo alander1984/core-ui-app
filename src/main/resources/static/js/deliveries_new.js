e9825cd07ab63384b1167fc4c7f2e04d7da9d27b
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-04-12 13:11:11 +0000

    CDS-79 part-1

--- src/main/resources/static/js/deliveries_new.js
+++ src/main/resources/static/js/deliveries_new.js
@@ -26,35 +26,12 @@ Vue.component('deliveries-new', {
                                 '<td>' +
                                     //'<b-button id="showItems" variant="primary"  @click="showItemsForDelivery(item.id)">Показать перечень продуктов</b-button><i class="fa fa-caret-down fa-lg"></i>' +
                                     '<i class="fa fa-location-arrow fa-lg compact_i w-50" style="cursor: pointer; padding-inline-start:5px; padding-inline-end:10px"></i>' +
-                                    '<i class="fa fa-clipboard-list fa-lg compact_i w-50" style="cursor: pointer; padding-inline-start:10px; padding-inline-end:5px" @click="showItemsForDelivery(item.id)"></i>' +                  
+                                    '<i class="fa fa-clipboard-list fa-lg compact_i w-50" style="cursor: pointer; padding-inline-start:10px; padding-inline-end:5px" @click="showDetailsForDelivery(item)"></i>' +                  
                                 '</td>' +
                             '</tr>' +
                         '</table>' +
                         '<div v-if="showInlineItems == true">' +
-                            '<h4>Список продуктов</h4>' +
-                            '<br /><br />' +
-                            '<table class="table table-bordered" style="width: 100%">' +
-                                '<tr>' +
-                                    '<th>№</th>' +
-                                    '<th>Наименование продукта</th>' +
-                                    '<th>Вес</th>' +
-                                    '<th>Ширина</th>' +
-                                    '<th>Высота</th>' +
-                                    '<th>Длина</th>' +
-                                    '<th>Количество</th>' +
-                                '</tr>' +
-                                '<tr v-for="(it, index2) in listItems" :key=it.index2 v-bind:style="{color: colorRow}">' +
-                                    '<td>{{index2 + 1}}</td>' +
-                                    '<td>{{it.productlmname}}</td>' +
-                                    '<td>{{it.weight}}</td>' +
-                                    '<td>{{it.width}}</td>' +
-                                    '<td>{{it.height}}</td>' +
-                                    '<td>{{it.length}}</td>' +
-                                    '<td>{{it.quantity}}</td>' +
-                                    '<td>{{it.quantity}}</td>' +
-                                '</tr>' +
-                            '</table>' + 
-                            '<b-button id="closeShowItems" variant="primary"  @click="closeShowItemsForDelivery()">Close</b-button>' +
+                            '<delivery-details-new v-bind:delivery="this.delivery"></delivery-details-new>' +
                         '</div>' +
                '</div>',
     data(){
@@ -62,6 +39,7 @@ Vue.component('deliveries-new', {
                 listDeliveries: [],
                 listItems: [],
                 selected: [],
+                delivery: {},
                 showInlineItems: false,
                 colorRow: 'black',
                 checked: false,
@@ -73,17 +51,22 @@ Vue.component('deliveries-new', {
         CDSAPI.Deliveries.sendNewDeliveries().then(deliveries => {
             
                 this.listDeliveries = deliveries;
+                this.delivery = this.listDeliveries[0];
                 console.log("Deliveries :  " + this.listDeliveries.length);
             });
     },
     methods: {
-        showItemsForDelivery(id){
-            //alert("Item " + id);
+        showDetailsForDelivery(item){
+            this.delivery = item;
+            console.log("Id --> " + this.delivery.id);
+            this.showInlineItems = true;
+            /*alert("Item " + id);
             CDSAPI.Deliveries.getItemsForDelivery(id).then(items => {
             
                 this.listItems = items;
                 console.log("Items :  " + this.listItems.length);
             });
+            */
         },
         closeShowItemsForDelivery(){
             this.showInlineItems = false;
