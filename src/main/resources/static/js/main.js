16038fb2ebcb5b8809c7df270cce57e27f48a513
Sergey Tkachenko <s.tkachenko@norbit.ru>
2019-04-15 09:34:20 +0000

    #

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,11 @@
-
+const store = new Vuex.Store({
+    state: {
+        delivery: {}
+    }
+})
 var app = new Vue({
     el: '#admin-app',
+    store,
     data: {
         message : 'This is Vue.js !!!!'
     }
