5400871a74a399b754c65e287787578fd15b31f7
sandro <zaynukov@gmail.com>
2019-04-16 13:00:21 +0300

    Добавлена страница логина

--- src/main/java/ru/cds/coreuiapp/controller/HomeController.java
+++ src/main/java/ru/cds/coreuiapp/controller/HomeController.java
@@ -1,6 +1,7 @@
 package ru.cds.coreuiapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,5 +13,10 @@ public class HomeController {
     public ModelAndView getHomeView() {
         ModelAndView modelAndView = new ModelAndView("index");
         return modelAndView;
+    }
+
+    @GetMapping(value="/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("log-in");
     }
 }
